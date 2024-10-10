b8c0d85d2ce749d5aa1cc3653d710186ad0ae198
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-10-10 11:31:33 -0300

    CombatActionUI: Script Desing

--- Micro-TurnBased/Assets/Scripts/CombatActionUI.cs
+++ Micro-TurnBased/Assets/Scripts/CombatActionUI.cs
@@ -9,6 +9,29 @@ public class CombatActionUI : MonoBehaviour
 {
    [SerializeField] private GameObject visualContainer;
    [SerializeField] private Button[] combatActionButtons;
+
+   private void OnEnable()
+   {
+      throw new NotImplementedException();
+   }
+
+   private void OnDisable()
+   {
+      throw new NotImplementedException();
+   }
+
+   void OnBeginTurn(Character character)
+   {
+      
+   }
    
-   
+   void OnEndTurn(Character character)
+   {
+      
+   }
+
+   public void OnClickCombatAction(CombatAction combatAction)
+   {
+      
+   }
 }
