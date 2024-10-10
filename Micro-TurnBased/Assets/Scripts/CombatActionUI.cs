b96333ab82ef511feb9ae1ae9a73cc143b28dc85
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-10-10 11:27:25 -0300

    CombatActionUI Properties

--- Micro-TurnBased/Assets/Scripts/CombatActionUI.cs
+++ Micro-TurnBased/Assets/Scripts/CombatActionUI.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CombatActionUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [SerializeField] private GameObject visualContainer;
+   [SerializeField] private Button[] combatActionButtons;
+   
+   
 }
