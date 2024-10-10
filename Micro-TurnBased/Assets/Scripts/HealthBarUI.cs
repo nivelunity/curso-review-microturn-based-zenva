e3b038f985b9bc2dd829d6d2f9717147c0b4ace0
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-10-10 14:46:28 -0300

    HealthBarUI Properties

--- Micro-TurnBased/Assets/Scripts/HealthBarUI.cs
+++ Micro-TurnBased/Assets/Scripts/HealthBarUI.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Image healthFill;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private Character character;
 }
