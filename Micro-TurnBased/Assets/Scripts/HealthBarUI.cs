520f6f1415c23a43f7b129442a8b39124c1ee830
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-10-10 14:49:41 -0300

    HealthBarUI  OnUpdateHealth

--- Micro-TurnBased/Assets/Scripts/HealthBarUI.cs
+++ Micro-TurnBased/Assets/Scripts/HealthBarUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,4 +10,25 @@ public class HealthBarUI : MonoBehaviour
     [SerializeField] private Image healthFill;
     [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private Character character;
+
+    private void OnEnable()
+    {
+        character.OnHealthChange += OnUpdateHealth;
+    }
+
+    private void OnDisable()
+    {
+        character.OnHealthChange -= OnUpdateHealth;
+    }
+
+    private void Start()
+    {
+        OnUpdateHealth();
+    }
+
+    void OnUpdateHealth()
+    {
+        healthFill.fillAmount = character.GetHealthPercentage();
+        healthText.text = character.CurHp + " / " + character.MaxHp;
+    }
 }
